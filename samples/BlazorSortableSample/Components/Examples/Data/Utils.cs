84eb229fb1c2ce22f687183388b1e2c8e60422c4
andhxl <123666356+andhxl@users.noreply.github.com>
2026-05-06 01:50:50 +0300

    fix(sample): update examples

--- samples/BlazorSortableSample/Components/Examples/Data/Utils.cs
+++ samples/BlazorSortableSample/Components/Examples/Data/Utils.cs
@@ -2,9 +2,10 @@ namespace BlazorSortableSample.Components.Examples.Data;
 
 public static class Utils
 {
-    public static IList<T> CreateItemList<T>(int count, int startValue = 1) where T : ClassBase, new()
+    public static IList<T> CreateItemList<T>(int count, int startValue = 1)
+        where T : ClassBase, new()
     {
-        var list = new List<T>();
+        var list = new List<T>(count);
 
         for (int i = 0; i < count; i++)
             list.Add(new T { Value = startValue++ });
